4df1dc37b51af8bd28c390dabab55f3539565448
Almas Akchabayev <almas.akchabayev@gmail.com>
2015-09-02 17:41:06 +0600

    successfully imported relay to react-native

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
+require('react-native-browser-polyfill');
 import React from 'react-native';
-//import Relay from 'react-relay';
+import Relay from '../relay/relay';
 import reactNativeRelayWebpackDemo from './components/App';
 
-//React.createElement(Relay.RootContainer, {
-//  Component: App,
-//  route: new _AppHomeRoute2['default'](),
-//})
+React.createElement(Relay.RootContainer, {
+  Component: App,
+  route: new _AppHomeRoute2['default'](),
+})
 
 React.AppRegistry.registerComponent('reactNativeRelayWebpackDemo', () => reactNativeRelayWebpackDemo);
